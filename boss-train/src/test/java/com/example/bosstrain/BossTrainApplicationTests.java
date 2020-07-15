266052de917a6950c5bbd4285a234e9bebba499d
752391183@qq.com <yqdhgqd999>
2020-07-15 11:44:04 +0800

    boss-trian项目初始化

---
+++ boss-train/src/test/java/com/example/bosstrain/BossTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bosstrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BossTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
